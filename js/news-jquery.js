b12e8a1be0bc7762976177c31260377c526b8b70
Lucas Souto <lssouto96@gmail.com>
2017-08-16 14:19:35 -0300

    ultimas_noticias_toggle

---
+++ js/news-jquery.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$(".content-oc").hide();
+});
+$(".item-1").mouseenter(function(){
+	$(".content-1").hide();
+	$(".content-oc-1").show();
+});
+$(".item-1").mouseleave(function(){
+	$(".content-oc-1").hide();
+	$(".content-1").show();
+});
+$(".item-2").mouseenter(function(){
+	$(".content-2").hide();
+	$(".content-oc-2").show();
+});
+$(".item-2").mouseleave(function(){
+	$(".content-oc-2").hide();
+	$(".content-2").show();
+});
+
+$(".item-3").mouseenter(function(){
+	$(".content-3").hide();
+	$(".content-oc-3").show();
+});
+$(".item-3").mouseleave(function(){
+	$(".content-oc-3").hide();
+	$(".content-3").show();
+});
+
