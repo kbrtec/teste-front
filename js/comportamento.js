11a25462fbb146d01ff4f4b2a3e6a876bb80b529
Roger Bernardo <rogerbernardo007@gmail.com>
2019-10-27 19:01:14 -0300

    Subindo arquivos

---
+++ js/comportamento.js
@@ -0,0 +1,7 @@
+function estrela_rosa(id) {
+    document.getElementById(id).src = "img/estrela_rosa.png";
+}
+
+function estrela_cinza(id) {
+    document.getElementById(id).src = "img/estrela_cinza.png";
+}
